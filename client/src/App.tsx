4ed90bad4f8015515ae27ce6e2c0bf0832ec90b8
Jake Johnson <jakejohnson801@outlook.com>
2023-02-17 13:45:20 -0600

    Setup client router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.sass';
 import logo from './logo.svg';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
